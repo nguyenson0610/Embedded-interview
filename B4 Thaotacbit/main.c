3da763c8109f0802ad7e41db4d42c49089214269
nguyenson0610 <116062501+nguyenson0610@users.noreply.github.com>
2023-05-16 23:54:56 +0700

    Update main.c

--- B4 Thaotacbit/main.c
+++ B4 Thaotacbit/main.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void readByte(uint8_t byte)
+{
+    printf("0b");
+    for (int i = 0; i < 8; i++)
+    {
+        uint8_t temp = 0b10000000;
+        uint8_t kq = byte & temp;
+        if (kq == 0)
+        {
+            printf("0");
+        }
+        else
+        {
+            printf("1");
+        }
+        byte = byte << 1;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    uint8_t test = 0b10010001;
+    readByte(test);
+    return 0;
+}
+
+
 
